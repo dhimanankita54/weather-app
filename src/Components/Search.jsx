93a688217981ff52ec8adfdde789c3c6ad11847f
dhimanankita54 <dhimanankita54@gmail.com>
2022-06-16 10:54:05 +0530

    default

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -8,32 +8,17 @@ import { Line } from "react-chartjs-2";
 export const Search = () => {
 
     const key = '00e1066df152855de0ff69878024b778';
-    const [city, setcity] = useState('');
+    const [city, setcity] = useState('Delhi');
     const [data, setdata] = useState([]);
     const [uptemps, settemps] = useState('');
 
-    async function weatherData(e) {
-        e.preventDefault();
-        if (city === "") {
-            alert("Add values");
-        } else {
-            const data = await fetch(
-                `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${key}`
-            )
-                .then((res) => res.json())
-                .then((data) => data);
-
-            setdata({ data: data });
-        }
-    }
-
-    const handleKeyPress = (e) => {
-        if (e.key === 'Enter') {
-            console.log('enter press here! ')
-            weatherData(e)
-        }
-    }
-
+ useEffect(() => {
+     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${key}`)
+     .then((res) => res.json())
+     .then((data) => {
+         setdata({data})
+     });
+ } ,[])   
 
     const getUpcomingTempForGraph = temps => {
         settemps(temps)
@@ -45,12 +30,9 @@ export const Search = () => {
             <input
                 className="input"
                 type='text'
-                placeholder='Enter Location'
                 value={city}
                 onChange={(e) => setcity(e.target.value)}
-                onKeyPress={(e) => handleKeyPress(e)}
             />
-
             {data.data !== undefined ? (
                 <div>
                     <UpcomingWeatherCardContainer location={city} />
