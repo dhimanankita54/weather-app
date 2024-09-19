b12771dcd7e17b8aac78109e78c8222f95ca9f19
Ankita Dhiman <86144377+dhimanankita54@users.noreply.github.com>
2024-09-19 09:43:27 +0530

    Update weather.js

--- src/Components/service/weather.js
+++ src/Components/service/weather.js
@@ -1,7 +1,7 @@
 import { DateTime } from "luxon";
 
 const API_KEY = "00e1066df152855de0ff69878024b778";
-const BASE_URL = "https://api.openweathermap.org/data/2.5";
+const BASE_URL = "https://api.openweathermap.org/data/3.0";
 
 // https://api.openweathermap.org/data/2.5/onecall?lat=48.8534&lon=2.3488&exclude=current,minutely,hourly,alerts&appid=1fa9ff4126d95b8db54f3897a208e91c&units=metric
 
